d18678cdea75a65c8d5b94370b8642a9fc328bb6
Yasmine Bouguerra <yasmine.bouguerra@esprit.tn>
2023-10-10 17:19:36 +0100

    DetailPub

--- app/src/main/java/tn/esprit/tourbuddy/ui/forum/ForumFragment.java
+++ app/src/main/java/tn/esprit/tourbuddy/ui/forum/ForumFragment.java
@@ -4,34 +4,76 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
-import tn.esprit.tourbuddy.databinding.FragmentForumBinding;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import tn.esprit.tourbuddy.R;
 
 public class ForumFragment extends Fragment {
 
-    private FragmentForumBinding binding;
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        ForumViewModel forumViewModel =
-                new ViewModelProvider(this).get(ForumViewModel.class);
-
-        binding = FragmentForumBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textForum;
-        forumViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
-    }
+    private ListView listView;
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_forum, container, false);
+
+        // Simulation de données
+        String[] titres = {"Publication 1", "Publication 2", "Publication 3"};
+        String[] contenus = {"Contenu de la publication 1", "Contenu de la publication 2", "Contenu de la publication 3"};
+        String[] auteurs = {"Auteur 1", "Auteur 2", "Auteur 3"};
+
+        ArrayList<HashMap<String, String>> listePublications = new ArrayList<>();
+
+        for (int i = 0; i < titres.length; i++) {
+            HashMap<String, String> publication = new HashMap<>();
+            publication.put("titre", titres[i]);
+            publication.put("contenu", contenus[i]);
+            publication.put("auteur", auteurs[i]);
+            listePublications.add(publication);
+        }
+
+        // Création d'un SimpleAdapter pour afficher les données dans la ListView
+        SimpleAdapter adapter = new SimpleAdapter(
+                getContext(),
+                listePublications,
+                android.R.layout.simple_list_item_2,
+                new String[]{"titre", "contenu", "auteur"},
+                new int[]{android.R.id.text1, android.R.id.text2}
+        );
+
+        listView = view.findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+
+        // Ajout d'un écouteur de clic pour les éléments de la liste
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                HashMap<String, String> publication = listePublications.get(position);
+                String titre = publication.get("titre");
+                String contenu = publication.get("contenu");
+                String auteur = publication.get("auteur");
+
+                // Naviguer vers le Fragment de détails avec les données de la publication
+                Bundle args = new Bundle();
+                args.putString("titre", titre);
+                args.putString("contenu", contenu);
+                args.putString("auteur", auteur);
+
+                NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment_content_main);
+                navController.navigate(R.id.nav_publication_details, args);
+            }
+        });
+
+        return view;
     }
 }
---
+++ app/src/main/java/tn/esprit/tourbuddy/ui/forum/PublicationDetailFragment.java
@@ -0,0 +1,38 @@
+package tn.esprit.tourbuddy.ui.forum;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import tn.esprit.tourbuddy.R;
+
+public class PublicationDetailFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_detailpub, container, false);
+
+        // Récupérer les détails de la publication à partir des arguments
+        Bundle bundle = getArguments();
+        if (bundle != null) {
+            String titre = bundle.getString("titre");
+            String contenu = bundle.getString("contenu");
+            String auteur = bundle.getString("auteur");
+
+            // Utiliser les détails de la publication pour mettre à jour l'interface utilisateur
+            TextView textViewTitre = view.findViewById(R.id.text_view_titre);
+            TextView textViewContenu = view.findViewById(R.id.text_view_contenu);
+            TextView textViewAuteur = view.findViewById(R.id.text_view_auteur);
+
+            textViewTitre.setText(titre);
+            textViewContenu.setText(contenu);
+            textViewAuteur.setText(auteur);
+        }
+
+        return view;
+    }
+}
